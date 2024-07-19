f01a249e5bd8de9ea9a8ccab16196fa8365deb17
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-07-19 09:20:50 +0200

    01/ Parent.java ++ ; Child.java ++ ; App.java update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,19 +1,21 @@
 package com.cc.java;
 
-public class App {
+public class App 
+{
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
      
+       Child child = new Child();
+        output(child.sayHello("Hi from "));
+        output(child.sayHello());
        
-       
-
     }
 
-    private static void output(String outputStr) {
+    private static void output(String outputStr) 
+    {
+
         System.out.println(outputStr);
+
     }
-
-
-
 }
-
